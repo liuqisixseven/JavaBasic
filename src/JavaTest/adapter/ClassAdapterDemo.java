333da52cbe3ecf332a7b9bd3c6139e8204ab787b
bqy0405 <1103921705@qq.com>
2022-07-11 18:53:39 +0800

    2022.7.11 18.53

---
+++ src/JavaTest/adapter/ClassAdapterDemo.java
@@ -0,0 +1,10 @@
+package JavaTest.adapter;
+
+public class ClassAdapterDemo {
+    public static void main(String[] args) {
+        Buqingyan buqingyan = new Buqingyan();
+        buqingyan.eat();
+        buqingyan.drink();
+        System.out.println("↑卜庆延");
+    }
+}
