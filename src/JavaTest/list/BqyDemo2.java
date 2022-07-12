9fc48d9a6ccf9163657151deebf579816221696d
bqy0405 <1103921705@qq.com>
2022-07-12 19:25:59 +0800

    2022.7.12 19.25

---
+++ src/JavaTest/list/BqyDemo2.java
@@ -0,0 +1,21 @@
+package JavaTest.list;
+
+public class BqyDemo2 {
+    public static void main(String[] args) {
+        Link k = new Link();
+        k.addNode("a");
+        k.addNode("o");
+        k.addNode("e");
+        k.addNode("i");
+        k.addNode("u");
+        System.out.println("韵母表");
+        k.printNode();
+        System.out.println();
+        k.deleteNode("o");
+        k.deleteNode("u");
+        System.out.println("删除两个");
+        k.printNode();
+        System.out.println();
+        System.out.println("韵母i存在情况：" + k.contains("i"));
+    }
+}
