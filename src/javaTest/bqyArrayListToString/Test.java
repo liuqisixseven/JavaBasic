8c70898e6061f1c2b202a440c82cbbba957664bd
bqy0405 <1103921705@qq.com>
2022-07-15 16:53:46 +0800

    2022.7.15 16:53

--- src/javaTest/bqyArrayListToString/Test.java
+++ src/javaTest/bqyArrayListToString/Test.java
@@ -6,7 +6,10 @@ public class Test {
     public static void main(String[] args) {
         ArrayList<String> arr2 = new ArrayList<>();
         arr2.add("100m");
+        arr2.add("200m");
         String a = arr2.toString();
+        String b = String.valueOf(arr2);
         System.out.println(a);
+        System.out.println(b);
     }
 }
