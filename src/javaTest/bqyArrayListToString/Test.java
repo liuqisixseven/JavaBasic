1ae18a91fa8318eb8f90f27ddaa9cd9cdf7134a4
bqy0405 <1103921705@qq.com>
2022-07-15 17:18:21 +0800

    2022.7.15 17:18

--- src/javaTest/bqyArrayListToString/Test.java
+++ src/javaTest/bqyArrayListToString/Test.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
+        /*
+        * 无效方法*/
         ArrayList<String> arr2 = new ArrayList<>();
         arr2.add("100m");
         arr2.add("200m");
@@ -11,7 +13,9 @@ public class Test {
         String b = String.valueOf(arr2);
         System.out.println(a.toString());
         System.out.println(b.toString());
-        
+
+        /*
+        * 使用String.append（）*/
         ArrayList<String> list = new ArrayList();
         list.add("one");
         list.add("two");
@@ -23,5 +27,18 @@ public class Test {
         }
         System.out.println(sb);
         System.out.println(sb.toString());
+
+        /*
+        * String.join()*/
+        ArrayList languages= new ArrayList<>();
+        // Add elements in the array list
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("JavaScript");
+        System.out.println("ArrayList: " + languages);
+
+        // convert the arraylist into a string
+        String arraylist = String.join("",languages);
+        System.out.println("String: " + arraylist);
     }
 }
