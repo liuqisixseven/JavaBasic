381fae132f884bfbe84df1bec4e16eebfaed538c
bqy0405 <1103921705@qq.com>
2022-07-15 17:06:22 +0800

    2022.7.15 16:53

--- src/javaTest/bqyArrayListToString/Test.java
+++ src/javaTest/bqyArrayListToString/Test.java
@@ -9,7 +9,19 @@ public class Test {
         arr2.add("200m");
         String a = arr2.toString();
         String b = String.valueOf(arr2);
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+        
+        ArrayList<String> list = new ArrayList();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        StringBuilder sb = new StringBuilder();
+        for (String s : list)
+        {
+            sb.append(s);
+        }
+        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
